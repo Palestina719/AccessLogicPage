b9a4886c8f90467689f5c4b47a5370e559d7784f
Palestina719 <m_palestina719@hotmail.com>
2016-08-04 18:41:34 -0500

    Diseño de los horarios por grupo

--- public/js/app.js
+++ public/js/app.js
@@ -10,3 +10,27 @@ $(window).bind("load", function () {
         });
     }
 });
+
+
+function calendario1(){
+var xmlhttp;
+
+if (window.XMLHttpRequest)
+{// code for IE7+, Firefox, Chrome, Opera, Safari
+xmlhttp=new XMLHttpRequest();
+}
+else
+{// code for IE6, IE5
+xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+}
+xmlhttp.onreadystatechange=function()
+{
+if (xmlhttp.readyState==4 && xmlhttp.status==200)
+{
+document.getElementById("WHorario").innerHTML=xmlhttp.responseText;
+}
+}
+xmlhttp.open("POST","views/1Grado",true);
+xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+xmlhttp.send("q=");
+}
