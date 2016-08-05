e1a75b75258aeb6578e587177cdbf76b2898b04e
Cesar Hernandez Antonio <hernandez.antonio.cesar19@gmail.com>
2016-08-04 20:31:31 -0500

    Agregada alerta para eliminar empleado y correciones a las vistas

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,24 @@
-$(window).bind("load", function () {
-    var footer = $("#footer");
-    var pos = footer.position();
-    var height = $(window).height();
-    height = height - pos.top;
-    height = height - footer.height();
-    if (height > 0) {
-        footer.css({
-            'margin-top': height + 'px'
-        });
-    }
+
+$(document).ready(function () {
+	
+	$(window).bind("load", function () {
+		var footer = $("#footer");
+		var pos = footer.position();
+		var height = $(window).height();
+		height = height - pos.top;
+		height = height - footer.height();
+		if (height > 0) {
+			footer.css({
+				'margin-top': height + 'px'
+			});
+		}
+	});
+	
+    var popAdd = new Foundation.Reveal($('#mdelUser'));
 });
+
+function delUsr(evt) {
+	var value = evt.target.getAttribute("value");
+	var frm = document.getElementById("frmDel");
+	frm.setAttribute("action", value);
+}
